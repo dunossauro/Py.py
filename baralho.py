27ad17572962580f8f0459098c450b4d5bce1f4d
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-05-13 11:41:30 -0300

    Create baralho.py

---
+++ baralho.py
@@ -0,0 +1,26 @@
+import random
+
+#Função que gera o baralho
+def gen ():
+	nipes = ["paus", "ouro", "espada", "copas"]
+
+	cartas = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+
+	baralho = []
+	for x in cartas:
+		for y in nipes:
+			gen_baralho = x,y
+			baralho.append(gen_baralho)
+	baralho.append("coringa")
+
+	return(baralho)
+
+def shuffle(baralho):
+	random.shuffle(baralho)
+	return(baralho)
+
+baralho = gen()
+embaralhado = shuffle(baralho)
+
+for cartas in embaralhado:
+	print(cartas)
