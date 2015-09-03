b0de1f75297ad098787a3e86d843c534ea878b59
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-09-03 13:16:06 -0300

    Update contador_de_palavras.py

--- NLTK_project/contador_de_palavras.py
+++ NLTK_project/contador_de_palavras.py
@@ -1,4 +1,12 @@
-import nltk
+#V: 0.0.2
+
+from nltk import Text,FreqDist,word_tokenize
+
+def plot(token, num=10):
+
+	texto = Text(token)
+
+	texto.plot(num)
 
 def contagem():
 
@@ -7,16 +15,16 @@ def contagem():
 	nome_s = input("Digite o nome do arquivo que deseja gravar a saída: ")
 
 	#Abertura do aquivo a ser lido
-	arquivo_e = open(nome_e	).read()
+	arquivo_e = open(nome_e).read()
 
 	#Abertura do arquivo de saida
 	arquivo_s = open(nome_s,"w")
 
 	#tokenização do arquivo
-	token = nltk.word_tokenize(arquivo_e)
+	token = word_tokenize(arquivo_e)
 
 	#Análise de frequência
-	num = nltk.FreqDist(token)
+	num = FreqDist(token)
 
 	#Exibição da contagem
 	num = num.most_common(len(num))
@@ -25,3 +33,9 @@ def contagem():
 		arquivo_s.write(("%s\t\t%s\n")%(y,x))
 
 	arquivo_s.close()
+
+	graf = input("Deseja exibir o gráfico das palavras mais frequentes? S/N: ")
+
+	if graf == 's' or graf == 'S':
+
+		plot(token)
