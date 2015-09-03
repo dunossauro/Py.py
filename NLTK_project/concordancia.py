f63e3a22e9755d90bc1afc2ecdda4b3169d14d57
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-09-03 13:15:49 -0300

    Update concordancia.py

--- NLTK_project/concordancia.py
+++ NLTK_project/concordancia.py
@@ -1,16 +1,22 @@
-import nltk, itertools
+#V: 0.0.2
+
+from nltk import Text,ConcordanceIndex,word_tokenize
+import  itertools
 
 #Base to function: https://simplypython.wordpress.com/2014/03/14/saving-output-of-nltk-text-concordance/
 def concordance_2_txt(nome_p, tokens, left_margin = 2, right_margin = 4):
      
-    text = nltk.Text(tokens)
-    c = nltk.ConcordanceIndex(text.tokens)
+    text = Text(tokens)
+    c = ConcordanceIndex(text.tokens)
     
     concordance_txt = ([text.tokens[list(map(lambda x: x-5 if (x-left_margin)>0 else 0,[offset]))[0]:offset+right_margin]
                         for offset in c.offsets(nome_p)])
                          
     return [''.join([x+' ' for x in con_sub]) for con_sub in concordance_txt]
 
+def plot(texto, palavra):
+
+	texto.dispersion_plot([palavra])
 
 def concord():
 
@@ -22,15 +28,15 @@ def concord():
 	arquivo_e = open(nome_e).read()
 	
 	#tokenização do arquivo
-	token = nltk.word_tokenize(arquivo_e)
+	token = word_tokenize(arquivo_e)
 
-	texto = nltk.Text(token)
+	texto = Text(token)
 
 	texto.concordance(nome_p)
 	
-	bool_ = input("Deseja salvar em um arquivo? S/N: ")
+	txt = input("Deseja salvar em um arquivo? S/N: ")
 
-	if bool_ == 's' or bool_ == 'S':
+	if txt == 's' or txt == 'S':
 
 		nome_s = input("Digite o nome do arquivo que deseja gravar a saída: ")
 
@@ -43,3 +49,9 @@ def concord():
 			arquivo_s.write(("%s\n")%(x))
 		
 		arquivo_s.close()
+
+	disp = input("Deseja exibir o gráfico de dispesão? S/N: ")
+
+	if disp == 's' or disp == 'S':
+
+		plot(texto,nome_p)
