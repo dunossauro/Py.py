e2483f6dfc4ccc5154635cc3b0174a033ff14a84
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-09-03 13:14:14 -0300

    Update app.py

--- NLTK_project/app.py
+++ NLTK_project/app.py
@@ -1,21 +1,22 @@
+#V: 0.0.2
+
 #Importação dos aquivos gerais.
 from os import system
 from contador_de_palavras import contagem
 from concordancia import concord
 from comparacao import compare
-"""
 from tagger import tag
-"""
+
 def main():
     system("clear")
     print("\
                  ####TNWST####\n\
             \n\
             \n\
-            1. Contador de palavras:\n\
-            2. Concordancia:\n\
+            1. Contador de palavras e Gráfico:\n\
+            2. Concordancia e Dispersão:\n\
             3. Comparação:\n\
-            4. Tagger:\n\n\
+            4. Tagger:(Inglês)\n\n\
             5. Sair\n\n")
 
     choice = int(input('Digite a opção desejada: '))
@@ -29,11 +30,11 @@ def main():
     elif choice == 3:
         compare()
     
-    elif choice == 5:
+    elif choice == 4:
         tag()
     
-    elif choice == 4:
-        exit
+    elif choice == 5:
+        exit()
 
 if __name__ == '__main__':
     main()
